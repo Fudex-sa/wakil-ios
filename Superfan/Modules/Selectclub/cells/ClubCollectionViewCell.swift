85193cc7d6cc31d27ec03de6ee77af3dfc7351a9
ADAM <MahmoudEzzet@fudex.com.sa>
2024-07-08 16:03:36 +0300

    no message

---
+++ Superfan/Modules/Selectclub/cells/ClubCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ClubCollectionViewCell.swift
+//  Superfan
+//
+//  Created by ADAM on 08/07/2024.
+//  Copyright © 2024 com.M.Abdu. All rights reserved.
+//
+
+import UIKit
+
+class ClubCollectionViewCell: BaseCollectionViewCell {
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var clubLbl: UILabel!
+    @IBOutlet weak var clubImg: UIImageView!
+}
